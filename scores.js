2a386a1d49ed74289f5fd0a5585fbddb46e8d4fc
ppruvost <patrick.pruvost50@gmail.com>
2026-05-06 14:56:43 +0200

    Add topScores array with player scores

---
+++ scores.js
@@ -0,0 +1,12 @@
+const topScores = [
+  { prenom: "Lucas", date: "06/05/2026", score: 980 },
+  { prenom: "Emma", date: "05/05/2026", score: 950 },
+  { prenom: "Nina", date: "05/05/2026", score: 920 },
+  { prenom: "Tom", date: "04/05/2026", score: 900 },
+  { prenom: "Léo", date: "04/05/2026", score: 880 },
+  { prenom: "Mila", date: "03/05/2026", score: 860 },
+  { prenom: "Enzo", date: "03/05/2026", score: 840 },
+  { prenom: "Jade", date: "02/05/2026", score: 820 },
+  { prenom: "Noah", date: "02/05/2026", score: 800 },
+  { prenom: "Lina", date: "01/05/2026", score: 780 }
+];
