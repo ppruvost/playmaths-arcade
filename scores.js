e6038f7d6f43e8da4e5c643e80429fcd3ab8277d
ppruvost <patrick.pruvost50@gmail.com>
2026-05-09 18:26:24 +0200

    Update topScores with new score values

--- scores.js
+++ scores.js
@@ -1,12 +1,12 @@
 const topScores = [
-  { prenom: "Lucas", date: "06/05/2026", score: 980 },
-  { prenom: "Emma", date: "05/05/2026", score: 950 },
-  { prenom: "Nina", date: "05/05/2026", score: 920 },
-  { prenom: "Tom", date: "04/05/2026", score: 900 },
-  { prenom: "Léo", date: "04/05/2026", score: 880 },
-  { prenom: "Mila", date: "03/05/2026", score: 860 },
-  { prenom: "Enzo", date: "03/05/2026", score: 840 },
-  { prenom: "Jade", date: "02/05/2026", score: 820 },
-  { prenom: "Noah", date: "02/05/2026", score: 800 },
-  { prenom: "Lina", date: "01/05/2026", score: 780 }
+  { prenom: "Lucas", date: "06/05/2026", score: 190 },
+  { prenom: "Emma", date: "05/05/2026", score: 180 },
+  { prenom: "Nina", date: "05/05/2026", score: 150 },
+  { prenom: "Tom", date: "04/05/2026", score: 130 },
+  { prenom: "Léo", date: "04/05/2026", score: 110 },
+  { prenom: "Mila", date: "03/05/2026", score: 100 },
+  { prenom: "Enzo", date: "03/05/2026", score: 100 },
+  { prenom: "Jade", date: "02/05/2026", score: 90 },
+  { prenom: "Noah", date: "02/05/2026", score: 80 },
+  { prenom: "Lina", date: "01/05/2026", score: 70 }
 ];
