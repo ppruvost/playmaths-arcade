75da5848bb68683e6131a5ff1fae401419203425
ppruvost <patrick.pruvost50@gmail.com>
2026-05-10 18:25:09 +0200

    Add CORS headers to save-score function

--- netlify/functions/save-score.js
+++ netlify/functions/save-score.js
@@ -1,3 +1,9 @@
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
+};
+
 const fetch = require("node-fetch");
 
 exports.handler = async (event) => {
