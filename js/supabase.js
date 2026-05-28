e0036425dd734d289398a78caabe4600e36b2b91
ppruvost <patrick.pruvost50@gmail.com>
2026-05-28 09:49:13 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL =
-"https://obsqakmhtvfuwnoxoksr.supabase.co/rest/v1/";
+"https://obsqakmhtvfuwnoxoksr.supabase.co";
 
 const SUPABASE_KEY =
 "sb_publishable_6FzuHhDBYOOSiAR9J-CiCA_KBfcyfhu";
