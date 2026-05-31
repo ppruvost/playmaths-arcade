4ccca137afbc3696be22c8bf62864dee61deda4c
ppruvost <patrick.pruvost50@gmail.com>
2026-05-31 21:37:00 +0200

    Update questions.js

--- automatisme/02/questions.js
+++ automatisme/02/questions.js
@@ -5,21 +5,21 @@
 window.questions = [
 
 {
-question:"Un article coûte 80 €. Hausse de 10 % ?",
+question:"Un article coûte 80 €. Hausse de 10 %. Quel est le prix final ?",
 options:["88 €","90 €","72 €","8 €"],
 bonne_reponse:"88 €",
 explication:"10 % de 80 = 8."
 },
 
 {
-question:"200 € avec remise de 20 %.",
+question:"200 € avec remise de 20 %. Quel est le prix final ?",
 options:["180 €","160 €","150 €","190 €"],
 bonne_reponse:"160 €",
 explication:"20 % de 200 = 40."
 },
 
 {
-question:"30 % de 50 ?",
+question:"30 % de 50.  Quel est le prix final ?",
 options:["10","12","15","20"],
 bonne_reponse:"15",
 explication:"0,30 × 50."
@@ -33,14 +33,14 @@ explication:"Coefficient multiplicateur 1,5."
 },
 
 {
-question:"TVA 20 % sur 100 €.",
+question:"TVA 20 % sur 100 € HT.  Quel est le prix TTC ?",
 options:["110 €","120 €","130 €","140 €"],
 bonne_reponse:"120 €",
 explication:"100 ×1,20."
 },
 
 {
-question:"Prix 50 €, baisse de 10 %.",
+question:"Prix 50 €, baisse de 10 %.  Quel est le prix final ?",
 options:["40 €","45 €","48 €","49 €"],
 bonne_reponse:"45 €",
 explication:"50−5."
@@ -54,14 +54,14 @@ explication:"Tout disparaît."
 },
 
 {
-question:"25 % de 80 ?",
+question:"25 % de 80 donne combien ?",
 options:["10","15","20","25"],
 bonne_reponse:"20",
 explication:"80 ÷4."
 },
 
 {
-question:"Graphique promotions.",
+question:"Graphique promotions. Quel est le pourcentage de la propotion en magasin ?",
 graphique:"promo1.png",
 options:["-10 %","-20 %","-30 %","-40 %"],
 bonne_reponse:"-20 %",
@@ -69,7 +69,7 @@ explication:"Lecture du panneau."
 },
 
 {
-question:"Lire le graphique d'évolution.",
+question:"Lire le graphique d'évolution. Mensuellement, quel est le pourcentage d'augmentation du prix",
 graphique:"courbe_prix1.png",
 options:["+5 %","+10 %","+20 %","+30 %"],
 bonne_reponse:"+10 %",
