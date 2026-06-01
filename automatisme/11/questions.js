8359c3712525085e931e4411c124861e04d08c70
ppruvost <patrick.pruvost50@gmail.com>
2026-06-01 09:37:20 +0200

    Update questions.js

--- automatisme/11/questions.js
+++ automatisme/11/questions.js
@@ -5,56 +5,56 @@
 window.questions = [
 
 {
-question:"3x=12",
+question:"soit 3x = 12, déterminer la valeur de x.",
 options:["2","3","4","6"],
 bonne_reponse:"4",
 explication:"12÷3."
 },
 
 {
-question:"x+7=10",
+question:"Soit x + 7 = 10, déterminer la valeur de x.",
 options:["2","3","4","17"],
 bonne_reponse:"3",
 explication:"10−7."
 },
 
 {
-question:"5x=25",
+question:"Soit 5x = 25, déterminer la valeur de x.",
 options:["4","5","6","10"],
 bonne_reponse:"5",
 explication:"25÷5."
 },
 
 {
-question:"Vrai ou Faux : x+2=5 ⇒ x=7",
+question:"Vrai ou Faux : x + 2 = 5 ⇒ x = 7",
 options:["Vrai","Faux"],
 bonne_reponse:"Faux",
-explication:"x=3."
+explication:"x = 3."
 },
 
 {
-question:"2x=18",
+question:"Soit 2x = 18, déterminer la valeur de x.",
 options:["8","9","10","12"],
 bonne_reponse:"9",
 explication:"18÷2."
 },
 
 {
-question:"x−4=8",
+question:"Soit x − 4 = 8, déterminer la valeur de x.",
 options:["2","4","12","16"],
 bonne_reponse:"12",
 explication:"+4."
 },
 
 {
-question:"Vrai ou Faux : résoudre 4x=20 donne x=5.",
+question:"Vrai ou Faux : résoudre 4x = 20 donne x = 5.",
 options:["Vrai","Faux"],
 bonne_reponse:"Vrai",
 explication:"20/4."
 },
 
 {
-question:"7+x=20",
+question:"Soit 7 + x = 20, déterminer la valeur de x.",
 options:["12","13","14","27"],
 bonne_reponse:"13",
 explication:"20−7."
